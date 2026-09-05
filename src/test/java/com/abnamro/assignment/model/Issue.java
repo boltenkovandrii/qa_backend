fbb4c1de86f64e5050152a6b6da67b5d7c6cc577
Andrii <boltenkovandrii@gmail.com>
2026-09-05 12:10:58 +0200

    Issue class formatting

--- src/test/java/com/abnamro/assignment/model/Issue.java
+++ src/test/java/com/abnamro/assignment/model/Issue.java
@@ -18,45 +18,62 @@ public record Issue(
         List<String> labels,
         int upvotes,
         int downvotes,
+        
         @JsonProperty("merge_requests_count")
         int mergeRequestsCount,
         String title,
+        
         @JsonProperty("updated_at")
         Instant updatedAt,
+        
         @JsonProperty("created_at")
         Instant createdAt,
+        
         @JsonProperty("closed_at")
         Instant closedAt,
+        
         @JsonProperty("closed_by")
         GitLabUser closedBy,
         boolean subscribed,
+        
         @JsonProperty("user_notes_count")
         int userNotesCount,
+        
         @JsonProperty("due_date")
         LocalDate dueDate,
         boolean imported,
+        
         @JsonProperty("imported_from")
         String importedFrom,
+        
         @JsonProperty("web_url")
         String webUrl,
         IssueReferences references,
+        
         @JsonProperty("time_stats")
         TimeStats timeStats,
         boolean confidential,
+        
         @JsonProperty("discussion_locked")
         Boolean discussionLocked,
+        
         @JsonProperty("issue_type")
         String issueType,
         String severity,
+        
         @JsonProperty("task_completion_status")
         TaskCompletionStatus taskCompletionStatus,
         Integer weight,
+        
         @JsonProperty("has_tasks")
         Boolean hasTasks,
+        
         @JsonProperty("_links")
         IssueLinks links,
+        
         @JsonProperty("moved_to_id")
         Integer movedToId,
+        
         @JsonProperty("service_desk_reply_to")
         String serviceDeskReplyTo
 ) {}
